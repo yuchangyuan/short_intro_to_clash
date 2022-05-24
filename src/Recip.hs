6426cdc054da2cf364b18dd2640386aff7638dc3
Yu Changyuan <reivzy@gmail.com>
2022-05-24 18:05:28 +0800

    src/Recip.hs: make Scalable UFixed synthesizable

--- src/Recip.hs
+++ src/Recip.hs
@@ -1,4 +1,4 @@
-{-# language FlexibleInstances, FunctionalDependencies #-}
+{-# language FlexibleInstances #-}
 module Recip where
 
 import Clash.Prelude
@@ -7,23 +7,62 @@ import Data.Ratio
 import qualified Data.List as L
 import qualified Prelude as P
 
-import Data.Ratio
+class (RealFrac a, Integral (ScaleIndex a), NFDataX (ScaleIndex a)) => Scalable a where
+  type ScaleIndex a :: Type
+  type instance ScaleIndex a = Integer
 
-class (RealFrac a, Integral b) => Scalable a b | a -> b where
-  unscale :: a -> (a, b)
+  -- only for x > 1, n = 0 if x <= 1
+  unscale :: a -> (a, ScaleIndex a)
   unscale x = let
     n = ceiling $ logBase 2 $ fromIntegral $ ceiling x
     y = x / (2 P.^ n)
     in (y, n)
 
-  scale :: a -> b -> a
+  scale :: a -> (ScaleIndex a) -> a
   scale y n = if n > 0 then y * 2 P.^ n else y / 2 P.^ (-n)
 
-instance Scalable Float Integer
-instance Scalable Double Integer
-instance (Integral a) => Scalable (Ratio a) Integer
+  -- NOTE: define scaleReverse as "scale y (-n)" by default, but this not always work
+  scaleReverse :: a -> (ScaleIndex a) -> a
+  scaleReverse y n = scale y $ negate n
 
-instance (KnownNat a, KnownNat b) => Scalable (UFixed a b) Integer
+instance Scalable Float
+instance Scalable Double
+instance (Integral a, NFDataX a) => Scalable (Ratio a)
+
+leadingZeroF :: (Num a) => (a, Bool) -> (a, Bool) -> (a, Bool)
+leadingZeroF (leftIdx, leftAllZero) (rightIdx, rightAllZero) =
+  if leftAllZero then (rightIdx, rightAllZero)
+  else (leftIdx, False)
+
+-- NOTE: for 0, return value is not correct
+leadingZero :: (KnownNat n) => BitVector (n + 1) -> Index (n + 1)
+leadingZero i = o where
+  bv = zipWith (\idx bit -> (idx, not $ bitCoerce bit)) indicesI (bv2v i)
+  o = fst $ fold leadingZeroF bv
+
+instance (KnownNat a, KnownNat b, a ~ (n + 1)) => Scalable (UFixed a b) where
+  type ScaleIndex (UFixed a b) = Index (a + b)
+  unscale x = (y, n) where
+    v = bitCoerce x
+    n = leadingZero v
+    p = snatToNum (SNat :: SNat a) -- length of integer part
+    y = if n > p
+        then x `shiftL` (fromIntegral $ n - p)
+        else x `shiftR` (fromIntegral $ p - n)
+
+  scale y n = x where
+    p = snatToNum (SNat :: SNat a)
+    x = if n > p
+        then y `shiftR` (fromIntegral $ n - p)
+        else y `shiftL` (fromIntegral $ p - n)
+
+  -- NOTE: here, scaleReverse y n != scale y (-n)
+  scaleReverse y n = x where
+    p = snatToNum (SNat :: SNat a)
+    x = if n > p
+        then y `shiftL` (fromIntegral $ n - p)
+        else y `shiftR` (fromIntegral $ p - n)
+
 
 recipStep d x = x * (2 - d * x)
 
@@ -35,8 +74,8 @@ recipI n d = L.foldr (.) id (L.replicate n $ recipStep d) $ 1
 -- recipI 4 3 :: Double
 -- recipI 6 3 :: Double
 
-recipFull :: Scalable a b => Int -> a -> a
-recipFull k x = scale (recipI k y) (-n)
+recipFull :: Scalable a => Int -> a -> a
+recipFull k x = scaleReverse (recipI k y) n
   where
     (y, n) = unscale x
 
